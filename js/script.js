396409870287f7306e2ec08a43962806971fd457
Josh <josh85035@gmail.com>
2026-06-26 17:57:17 -0500

    display text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+let play = document.querySelector(".play");
+let userChoice = document.querySelector(".userChoice");
 
+play.addEventListener("click",function(){
+    let input = document.querySelector(".input");
+    userChoice.innerHTML = input.value;
+});
