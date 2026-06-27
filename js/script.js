0753723db6c22148f3900667333e9e283464e26b
Josh <josh85035@gmail.com>
2026-06-27 12:47:29 -0500

    results

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let play = document.querySelector(".play");
 let userChoice = document.querySelector(".userChoice");
+let result = document.querySelector(".result");
 let randomNumber = 0;
 let computerChoice = document.querySelector(".computerChoice");
 
 play.addEventListener("click", function () {
     randomNumber = Math.random();
     let input = document.querySelector(".input");
+
+    // take input and make sure it is rock, paper, or scissors
     if (input.value.trim() === ""){
         userChoice.innerHTML = "Choose between Rock, Paper, or Scissors.";
     } else if (input.value.trim().toLowerCase() === "rock" ||
@@ -16,11 +19,26 @@ play.addEventListener("click", function () {
         userChoice.innerHTML = "Choose between Rock, Paper, or Scissors."
     }
 
+    //generate psuedorandom chance for rock, paper, or scissors
     if (randomNumber < 0.33){
-        computerChoice.innerHTML = "Rock";
+        computerChoice.innerHTML = "rock";
     } else if (randomNumber > 0.66){
-        computerChoice.innerHTML = "Paper";
+        computerChoice.innerHTML = "paper";
     } else {
-        computerChoice.innerHTML = "Scissors";
+        computerChoice.innerHTML = "scissors";
     }
+
+    //see who wins
+    if (input.value.trim().toLowerCase() === "rock" && computerChoice.innerHTML==="scissors" ||
+        input.value.trim().toLowerCase() === "scissors" && computerChoice.innerHTML === "paper" ||
+        input.value.trim().toLowerCase() === "paper" && computerChoice.innerHTML === "rock")
+    {
+        result.innerHTML = "You Win!";
+    } else if (input.value.trim().toLowerCase() === computerChoice.innerHTML){
+        result.innerHTML="You Tie!";
+    } else {
+        result.innerHTML = "You Lose";
+    }
+    
+    input.value="";
 });
