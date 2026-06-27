6e290b4fc3a963e8c761f91af365d20c310dc08c
Josh <josh85035@gmail.com>
2026-06-27 10:32:58 -0500

    random number

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 let play = document.querySelector(".play");
 let userChoice = document.querySelector(".userChoice");
+let randomNumber = 0;
+let computerChoice = document.querySelector(".computerChoice");
 
-play.addEventListener("click",function(){
+play.addEventListener("click", function () {
+    randomNumber = Math.random();
     let input = document.querySelector(".input");
     userChoice.innerHTML = input.value;
+    computerChoice.innerHTML = randomNumber;
 });
