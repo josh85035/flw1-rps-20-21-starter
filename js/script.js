7a7e233baed07f3017ce0cd304c61d891261ec64
Josh <josh85035@gmail.com>
2026-06-27 11:16:17 -0500

    user choice

--- js/script.js
+++ js/script.js
@@ -6,6 +6,21 @@ let computerChoice = document.querySelector(".computerChoice");
 play.addEventListener("click", function () {
     randomNumber = Math.random();
     let input = document.querySelector(".input");
-    userChoice.innerHTML = input.value;
-    computerChoice.innerHTML = randomNumber;
+    if (input.value.trim() === ""){
+        userChoice.innerHTML = "Choose between Rock, Paper, or Scissors.";
+    } else if (input.value.trim().toLowerCase() === "rock" ||
+               input.value.trim().toLowerCase() === "paper" ||
+               input.value.trim().toLowerCase() === "scissors"){
+        userChoice.innerHTML = input.value;
+    } else {
+        userChoice.innerHTML = "Choose between Rock, Paper, or Scissors."
+    }
+
+    if (randomNumber < 0.33){
+        computerChoice.innerHTML = "Rock";
+    } else if (randomNumber > 0.66){
+        computerChoice.innerHTML = "Paper";
+    } else {
+        computerChoice.innerHTML = "Scissors";
+    }
 });
